d0c616b39384a66a284d8ca92e49320b975c25bc
Marcos Pereira <marcospere@gmail.com>
2021-05-06 12:14:04 -0300

    First commit

---
+++ src/dashboard.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./dashboard.css";
+import Chart from "react-google-charts";
+
+function Dashboard() {
+  const TITULO = "Quantidade de cadastros primeiro semestre";
+
+  const [dados, setDados] = useState([
+    ["Mês", "Quantidade"],
+    ["Janeiro", 33],
+    ["Fevereiro", 68],
+    ["Março", 49],
+    ["Abril", 15],
+    ["Maio", 80],
+    ["Junho", 27],
+  ]);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Chart
+        width={"400px"}
+        height={"300px"}
+        chartType={"PieChart"}
+        data={dados}
+        options={{
+          title: TITULO,
+        }}
+      />
+      <Chart
+        width={"400px"}
+        height={"300px"}
+        chartType={"PieChart"}
+        data={dados}
+        options={{
+          title: TITULO,
+          is3D: true,
+        }}
+      />
+      <Chart
+        width={"400px"}
+        height={"300px"}
+        chartType={"PieChart"}
+        data={dados}
+        options={{
+          title: TITULO,
+          pieHole: 0.4,
+        }}
+      />
+      <Chart
+        width={"400px"}
+        height={"300px"}
+        chartType={"BarChart"}
+        data={dados}
+        options={{
+          title: TITULO,
+          chartArea: { width: "50%" },
+          hAxis: { title: "Quantidade" },
+          vAxis: { title: "Mês" },
+          animation: { duration: 1000, easing: "out", startup: true },
+        }}
+      />
+      <Chart
+        width={"400px"}
+        height={"300px"}
+        chartType={"LineChart"}
+        data={dados}
+        options={{}}
+      />
+    </div>
+  );
+}
+
+export default Dashboard;
